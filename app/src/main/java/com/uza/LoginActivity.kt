301d6e94d9a3955eb0a13b5300ba8798e77f81e5
shazomii <seyeoyeniran2u@gmail.com>
2020-10-15 14:46:03 +0100

    Login fixes

--- app/src/main/java/com/uza/LoginActivity.kt
+++ app/src/main/java/com/uza/LoginActivity.kt
@@ -1,25 +1,41 @@
 package com.uza
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         auth = FirebaseAuth.getInstance()
+
+        // Configure Google Sign-In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
         // Register a new user
         buttonSignup.setOnClickListener {
@@ -31,26 +47,63 @@ class LoginActivity : AppCompatActivity() {
         buttonLogin.setOnClickListener {
             loginUser()
         }
+
+        googleSignIn.setOnClickListener {
+            googleSignIn()
+        }
+    }
+
+    private fun googleSignIn() {
+        val intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(intent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+
+                updateUIWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Toast.makeText(baseContext, getString(R.string.signInFailed) + e.statusCode, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onStart() {
         super.onStart()
         val currentUser = auth.currentUser
         updateUI(currentUser)
+
+    }
+
+    private fun updateUIWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Toast.makeText(baseContext, getString(R.string.signInFailed), Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
     }
 
     private fun updateUI(currentUser: FirebaseUser?) {
-        if (currentUser != null ) {
-            if (currentUser.isEmailVerified) {
-                startActivity(Intent(this, MainActivity::class.java))
-                finish()
-            } else {
-                Toast.makeText(baseContext, getString(R.string.verifyMail), Toast.LENGTH_SHORT).show()
+            if (currentUser != null) {
+                if (currentUser.isEmailVerified) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }
             }
-        }
     }
 
     private fun loginUser() {
+
         if (userEmailLogin.text.toString().isEmpty()) {
             userEmailLogin.error = getString(R.string.emailEmptyError)
             userEmailLogin.requestFocus()
@@ -68,6 +121,7 @@ class LoginActivity : AppCompatActivity() {
             userPassword.requestFocus()
             return
         }
+        buttonLogin.text = getString(R.string.logging_in)
 
         auth.signInWithEmailAndPassword(userEmailLogin.text.toString(), userPassword.text.toString())
             .addOnCompleteListener(this) { task ->
