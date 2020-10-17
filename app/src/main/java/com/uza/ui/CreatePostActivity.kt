345d86fd16b85fd5a09d3a401b8b38fcdf8e5914
paul Odhiambo <paulodhiambo962@gmail.com>
2020-10-17 20:42:48 +0300

    createPost layout

--- app/src/main/java/com/uza/ui/CreatePostActivity.kt
+++ app/src/main/java/com/uza/ui/CreatePostActivity.kt
@@ -1,12 +1,16 @@
 package com.uza.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.uza.R
+import kotlinx.android.synthetic.main.activity_create_post.*
+
 
 class CreatePostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
+        btn_pick_image.setOnClickListener {
+        }
     }
 }
